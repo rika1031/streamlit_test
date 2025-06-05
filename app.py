cf74f5a6a168b23db9f940dad393a473cc8e7393
rika1031 <cctmropqr@gmail.com>
2025-06-06 00:15:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-import pandas as pd
 import streamlit as st
 
 # タイトル
-st.title("売上データの表示")
-
-# Excelファイルの読み込み
-df = pd.read_excel("data.xlsx")
-
-# 表を表示
-st.write("データ", df)
+st.title("This is a test page that rika has made.")
