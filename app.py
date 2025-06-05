33f6f2ccaa7e8dffbc1b61a5297da4ef6aee8d43
rika1031 <cctmropqr@gmail.com>
2025-06-06 00:08:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import pandas as pd
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("これはとても簡単な Streamlit アプリです！")
+# タイトル
+st.title("売上データの表示")
 
-# ↓以下を追加
-file_buffer = st.file_uploader("ファイルをアップロードしてください")
+# Excelファイルの読み込み
+df = pd.read_excel("data.xlsx")
+
+# 表を表示
+st.write("データ", df)
